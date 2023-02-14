203516fee889fe687bab75dcb05ac7805a4cd2cf
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-14 10:56:51 +0900

    SimpleEcho_morden

--- src/day21/SimpleEchoClient.java
+++ src/day21/SimpleEchoClient.java
@@ -4,21 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class SimpleEchoClient {
     public static void main(String[] args) {
         System.out.println("에코 클라이언트 시작됨");
         try {
-            InetAddress localAddress = InetAddress.getLocalHost();  // 127.0.0.1
+            //InetAddress localAddr = InetAddress.getLocalHost();  // 127.0.0.1
             Socket clientSocket = null;
             PrintWriter pw = null;
             BufferedReader br = null;
             try{
-                clientSocket = new Socket("192.168.56.1", 10000);
+                clientSocket = new Socket("127.0.0.1", 9900);
                 pw = new PrintWriter(clientSocket.getOutputStream(), true);
                 br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
@@ -32,14 +30,14 @@ public class SimpleEchoClient {
                     }
                     pw.println(line);  // 서버로 전송
                     // 서버로 부터 수신받은 객체에서 라인 단위로 문자열 리턴
-                    System.out.println("서버로 부터 받은 메아리 : " + br.readLine());
+                    System.out.println("서버로 부터 받은 메세지 : " + br.readLine());
                 }
             } catch (IOException ex) {
                 System.out.println("입출력 예외 발생");
             } finally {  // 자원 해제
-                clientSocket.close();
-                pw.close();
-                br.close();
+                if (clientSocket != null) clientSocket.close();
+                if (pw != null) pw.close();
+                if (br != null) br.close();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/day21/SimpleEchoServer.java
+++ src/day21/SimpleEchoServer.java
@@ -6,31 +6,38 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class SimpleEchoServer {
     public static void main(String[] args) {
         System.out.println("에코 서버 시작됨");
-        try (ServerSocket serverSocket = new ServerSocket(6000)) { // ServerSocket 객체 생성 (포트 바인딩 : 6000번)
+        try (ServerSocket serverSocket = new ServerSocket(9900)) {
             System.out.println("클라이언트 접속 대기 중.....");
-            Socket clientSocket = serverSocket.accept();  // 접속 대기 --> accept
+            Socket clientSocket = serverSocket.accept();  // 접속 대기
             System.out.println("클라이언트 접속됨.");
 
-            BufferedReader br = null;
-            PrintWriter pw = null;
-
-            try {
-                br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                pw = new PrintWriter(clientSocket.getOutputStream(), true); //true -> 버퍼 자동 생성
-                String line;
-                while ((line = br.readLine()) != null) {
-                    System.out.println("클라이언트로 부터 받은 메세지 : " + line);
-                    pw.println(line);  // 클라이언트로 송신
-                }
+            try (
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(clientSocket.getInputStream()));
+                    PrintWriter pw =
+                            new PrintWriter(clientSocket.getOutputStream(), true))
+            {
+                Supplier<String> socketIn = () -> {
+                    try {
+                        return br.readLine();
+                    } catch (IOException ex) {
+                        return null;
+                    }
+                };
+                Stream s = Stream.generate(socketIn);
+                s.map(text -> {
+                    System.out.println("클라이언트로 부터 받은 메세지 : " + text);
+                    pw.println(text);
+                    return text;
+                }).allMatch(t -> t != null);
             } catch (IOException e) {
                 throw new RuntimeException(e);
-            } finally {
-                br.close();
-                pw.close();
             }
         }
         catch (IOException ex) {
