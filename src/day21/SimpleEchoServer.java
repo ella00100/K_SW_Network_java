39a9616c43e16a2036898c28c7f9e0e29ad110a6
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-14 14:16:29 +0900

    SimpleEcho_thread

--- src/day21/SimpleEchoServer.java
+++ src/day21/SimpleEchoServer.java
@@ -7,41 +7,49 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
-public class SimpleEchoServer {
-    public static void main(String[] args) {
-        System.out.println("에코 서버 시작됨");
-        try (ServerSocket serverSocket = new ServerSocket(9900)) { //서버 소켓 생성
-            System.out.println("클라이언트 접속 대기 중.....");
-            Socket clientSocket = serverSocket.accept();  // 클라이언트의 요청을 받아 Socket 객체 생성
-            System.out.println("클라이언트 접속됨.");
-
-            try (
-                    BufferedReader br = new BufferedReader(
-                            new InputStreamReader(clientSocket.getInputStream()));
-                    PrintWriter pw =
-                            new PrintWriter(clientSocket.getOutputStream(), true))
-            {
-                Stream
-                        .generate(() -> {
-                            try {
-                                return br.readLine();
-                            } catch (IOException ex) {
-                                return null;
-                            }
-                        })
-                        .peek(text -> {
-                            System.out.println("클라이언트로 부터 받은 메세지 : " + text);
-                            pw.println(text);
-                        })
-                        .allMatch(Objects::nonNull);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+public class SimpleEchoServer implements Runnable {
+    private static Socket clientSocket;
+    public SimpleEchoServer(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+    public static void main(String[ ] args){
+        ExecutorService eService = Executors.newFixedThreadPool(2);
+        System.out.println("다중 접속 에코 서버");
+        try (ServerSocket serverSocket = new ServerSocket(20000)) {
+            while (true) {
+                System.out.println("클라이언트 접속 대기중.....");
+                clientSocket = serverSocket.accept();
+                SimpleEchoServer tes = new SimpleEchoServer(clientSocket);
+                eService.submit(tes);
             }
+        } catch (IOException ex) {
+            // Handle exceptions
         }
-        catch (IOException ex) {
-            System.out.println("접속 실패!");
+        System.out.println("다중 접속 에코 서버 종료");
+        eService.shutdown();
         }
+
+        @Override
+        public void run() {
+            System.out.println("[" + Thread.currentThread() + "] 스레드 : ");
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+                 PrintWriter out =
+                         new PrintWriter(clientSocket.getOutputStream(), true)) {
+                String inputLine;
+                while ((inputLine = br.readLine()) != null) {
+                    System.out.println("[" + Thread.currentThread() + "]" + "클라이언트가 보낸 메세지 : " + inputLine);
+                    out.println(inputLine);
+                }
+                System.out.println("[" + Thread.currentThread() + "] 클라이언트 종료됨");
+            } catch (IOException ex) {
+                System.out.println("입출력 예외 발생");
+            }
     }
 }
+
+
