b709bb7ed3c6853cfbf0097960a0afec72defa52
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-14 18:00:50 +0900

    thread ãHW1

--- src/day22/Thread1.java
+++ src/day22/Thread1.java
@@ -1,12 +1,13 @@
 package day22;
 
-class WorkerThread extends Thread{
+class Worker extends Thread{
     public void run(){
-        for (int i = 0; i < 5; i++){
-            System.out.print("잘가");
-            try{
+        for (int i = 0; i<5; i++){
+            System.out.println("작업 스레드 : "+ i);
+            try {
                 Thread.sleep(500);
             }catch (InterruptedException e){
+                System.out.println("오류 발생");
             }
         }
     }
@@ -14,15 +15,20 @@ class WorkerThread extends Thread{
 
 public class Thread1{
     public static void main(String[] args){
-        Thread t = new WorkerThread();
-        t.start();
 
-        for (int i =0; i < 5; i++){
-            System.out.println("안녕");
-            try{
-                Thread.sleep(500);
-            }catch (InterruptedException e) {
-            }
-        }
+        Thread t = new Worker();   //작업 스레드 객체 생성
+        t.start();                 // 작업 스레드 작업 시작
+        int alphabet = 'a';
+
+        while (t.isAlive()){        //작업 스레드가 살아있는 동안
+             System.out.print("메인 스레드 : ");
+             System.out.println((char) (alphabet));
+             alphabet++;
+             try {
+                 Thread.sleep(500);
+             } catch (InterruptedException e) {
+                 System.out.println("오류 발생");
+             }
+         }
     }
 }
