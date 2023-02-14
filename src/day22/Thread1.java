f61e9b2b2592731ec981ff056a3c2c247204cbba
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-14 17:23:03 +0900

    thread 02

--- src/day22/Thread1.java
+++ src/day22/Thread1.java
@@ -1,12 +1,9 @@
 package day22;
 
-public class Thread1 {
-    public static void main(String[] args){
-        Thread t = new Thread(new MyRunnable());
-        t.start();
-
-        for(int i = 0; i < 5; i++){
-            System.out.print("안녕 ");
+class WorkerThread extends Thread{
+    public void run(){
+        for (int i = 0; i < 5; i++){
+            System.out.print("잘가");
             try{
                 Thread.sleep(500);
             }catch (InterruptedException e){
@@ -15,13 +12,16 @@ public class Thread1 {
     }
 }
 
-class MyRunnable implements Runnable {
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("잘가");
-            try {
+public class Thread1{
+    public static void main(String[] args){
+        Thread t = new WorkerThread();
+        t.start();
+
+        for (int i =0; i < 5; i++){
+            System.out.println("안녕");
+            try{
                 Thread.sleep(500);
-            } catch (InterruptedException e) {
+            }catch (InterruptedException e) {
             }
         }
     }
