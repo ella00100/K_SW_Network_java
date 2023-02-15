6f72558ebc09757ce715e2501bf4521c6d0d7e33
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-15 15:22:47 +0900

    day23 generate Objects

--- src/day23/Test.java
+++ src/day23/Test.java
@@ -5,33 +5,17 @@ import java.net.NetworkInterface;
 import java.net.UnknownHostException;
 import java.net.SocketException;
 
-public class Test {
-    public static String getMACIdentifier(NetworkInterface network){
-        StringBuilder identifier = new StringBuilder();
-        try {
-            byte[] macBuffer = network.getHardwareAddress();
-            if (macBuffer != null) {
-                for (int i = 0; i < macBuffer.length; i++) {
-                    identifier.append(
-                            String.format("%02X%s",macBuffer[i], (i < macBuffer.length - 1) ? "-" : ""));
-                }
-            } else {
-                return "---";
-            }
-        } catch (SocketException ex) {
-            ex.printStackTrace();
-        }
-        return identifier.toString();
+class One{
+    private One(){
     }
+    public static One produceOneInstance(){
+        return new One();
+    }
+}
 
-    public static void main(String[] args) throws SocketException {
-        InetAddress addr = null;
-        try{
-            addr = InetAddress.getLocalHost();
-            System.out.println("IP address: " + addr.getHostAddress());
-            NetworkInterface network = NetworkInterface.getByInetAddress(addr);
-            System.out.println("MAC address:" + getMACIdentifier(network));
-        }catch(UnknownHostException e) {
-        }
+public class Test {
+    public static void main(String [] args){
+        One o = One.produceOneInstance();
+        One o2 = One.produceOneInstance();
     }
 }
