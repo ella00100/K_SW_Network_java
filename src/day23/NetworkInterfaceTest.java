92c7ed6de392434af54149fb08355c86ecc6a238
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-15 13:35:40 +0900

    day23 NetworkInterface02

--- src/day23/NetworkInterfaceTest.java
+++ src/day23/NetworkInterfaceTest.java
@@ -4,6 +4,7 @@ import java.net.NetworkInterface;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.net.SocketException;
+import java.net.InetAddress;
 
 public class NetworkInterfaceTest {
     public static void main(String[] args){
@@ -12,6 +13,10 @@ public class NetworkInterfaceTest {
             System.out.printf("이름        디스플레이 이름\n");
             for (NetworkInterface element : Collections.list(interfaceEnum)) {
                 System.out.printf("%-10s %-50s\n", element.getName(), element.getDisplayName());
+                Enumeration<InetAddress> addresses = element.getInetAddresses();
+                for (InetAddress inetAddress : Collections.list(addresses)) {
+                    System.out.printf("---InetAddress: %s\n", inetAddress);
+                }
             }
         } catch(SocketException ex){
             System.out.println();
