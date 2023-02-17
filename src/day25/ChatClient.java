ed2e1156358953146b3e93553767ada555b02493
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-17 11:42:55 +0900

    day25 chatServer/Client Fixed vs variable

--- src/day25/ChatClient.java
+++ src/day25/ChatClient.java
@@ -8,21 +8,31 @@ import java.util.Scanner;
 
 public class ChatClient {
     public static void main(String[] args){
-        SocketAddress address = new InetSocketAddress("127.0.0.1", 5000);
-        try (SocketChannel socketChannel = SocketChannel.open(address)) {
-            System.out.println("서버와 연결됨");
-                    String message;
-            Scanner scanner = new Scanner(System.in);
+        SocketAddress addr = new InetSocketAddress("127.0.0.1", 5000);
+        try (
+                SocketChannel sc = SocketChannel.open(addr)) { //Socket Channel 열기
+                System.out.println("서버와 연결됨");
+
+                String message;
+                Scanner scanner = new Scanner(System.in);
+
             while (true) {
+                // 서버에서 보낸 메세지 받아오기 (고정된 메세지 공간)
                 System.out.println("서버로 부터의 메세지 대기중.. ");
-                System.out.println("Message: " + HelperMethods.receiveFixedLengthMessage(socketChannel));
+                //System.out.println("Message: " + HelperMethods.receiveFixedLengthMessage(sc));
+                System.out.println("Message: " + HelperMethods.receiveMessage(sc));
+
+                // 메세지 보내기
                 System.out.print("> ");
                 message = scanner.nextLine();
+                //만약 quit을 입력하면 클라이언트 종료
                 if (message.equalsIgnoreCase("quit")) {
-                    HelperMethods.sendFixedLengthMessage(socketChannel, "클라이언트 종료");
+                    //HelperMethods.sendFixedLengthMessage(sc, "클라이언트 종료");
+                    HelperMethods.sendMessage(sc, "클라이언트 종료");
                     break;
                 }
-                HelperMethods.sendFixedLengthMessage(socketChannel, message);
+                //HelperMethods.sendFixedLengthMessage(sc, message);
+                HelperMethods.sendMessage(sc, message);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
--- src/day25/ChatServer.java
+++ src/day25/ChatServer.java
@@ -1,6 +1,7 @@
 package day25;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
@@ -10,27 +11,35 @@ public class ChatServer {
     public static void main(String[] args) {
         System.out.println("Char Server 시작");
         try {
-            ServerSocketChannel ssc = ServerSocketChannel.open();
-            ssc.socket().bind(new InetSocketAddress(5000));
+            ServerSocketChannel ssc = ServerSocketChannel.open(); //ServerSocketChannel 열기
+            ssc.socket().bind(new InetSocketAddress(5000));  //5000번 포트에 bind
             boolean running = true;
             while (running) {
                 System.out.println("클라이언트 접속 대기 중...");
-                SocketChannel socketChannel = ssc.accept();
+                SocketChannel sc = ssc.accept(); // 연결 전까지 blocking
                 System.out.println("클라이언트 연결됨");
 
                 String msg;
                 Scanner scanner = new Scanner(System.in);
                 while (true) {
+                    //메세지 보내기
                     System.out.print("> ");
-                    msg = scanner.nextLine();
-                    if (msg.equalsIgnoreCase("quit")) {
-                        HelperMethods.sendFixedLengthMessage(socketChannel, "서버 종료");
+                    msg = scanner.nextLine(); // 키보드 입력 받아오기
+
+                    //만약 quit을 입력하면 서버 종료
+                    if (msg.equalsIgnoreCase("quit")) { //quit을 입력하면 종료
+                        //HelperMethods.sendFixedLengthMessage(sc, "서버 종료");
+                        HelperMethods.sendMessage(sc, "서버 종료");
                         running = false;
                         break;
                     } else {
-                        HelperMethods.sendFixedLengthMessage(socketChannel, msg);
+                        //HelperMethods.sendFixedLengthMessage(sc, msg);
+                        HelperMethods.sendMessage(sc, msg);
+
+                        /// 클라이언트에서 메세지 받아오기
                         System.out.println("클라이언트로부터의 메세지 대기 중...");
-                        System.out.println("Message: " + HelperMethods.receiveFixedLengthMessage(socketChannel));
+                        //System.out.println("Message: " + HelperMethods.receiveFixedLengthMessage(sc));
+                        System.out.println("Message: " + HelperMethods.receiveMessage(sc));
                     }
                 }
             }
