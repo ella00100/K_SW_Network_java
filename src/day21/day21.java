42eaccc9e8f4fc07287e46453a6d944be75660c1
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-20 10:08:02 +0900

    URL connection

--- src/day21/day21.java
+++ src/day21/day21.java
@@ -1,28 +1,37 @@
 package day21;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.MalformedURLException;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 public class day21 {
     public static void main(String[] args){
         try {
             URL url = new URL("http://www.inha.ac.kr");     // URL 객체 생성
             URLConnection urlConnection = url.openConnection();  // URLConnection 객체 생성
-            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            InputStream inputstream = urlConnection.getInputStream();
+
+            //ReadableByteChannel은 read메소드를 이용해 사이트에서 읽기 작업이 가능하게 함
+            ReadableByteChannel channel = Channels.newChannel(inputstream);
+
+            //ByteBuffer는 채널에서 데이터 수신
+            ByteBuffer br = ByteBuffer.allocate(64);
             String line = null;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
+
+            //read메소드는 읽은 바이트 수 반환
+            while (channel.read(br) > 0) {
+                System.out.println(new String(br.array()));
+                br.clear();
             }
-            br.close();
-        }catch  (MalformedURLException e){
-            throw new RuntimeException(e);
-        }
-        catch (IOException ex) {
-            System.out.println("test");
+            channel.close();
+        } catch (IOException ex) {
         }
     }
 }
